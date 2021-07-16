98f67e2c860c77123e7b0344bcb5d90b977bc88f
root <root@Zara-Parse.local>
2021-07-16 11:57:37 +0000

    fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import asyncio
 import logging.config
+import aiohttp
+from aiohttp_proxy import ProxyConnector
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
+from aiogram.utils.executor import start_webhook, start_polling
 from config import API_TOKEN, WEBHOOK_HOST, WEBHOOK_PATH, WEBAPP_PORT, admins
 from db import db_init, Url
 from logger_config import logger_config
@@ -78,9 +80,9 @@ async def send_info(data, del_size=None, new_size=None):
         await bot.send_message(admin, text)
 
 
-async def parse_url(url: Url):
+async def parse_url(url: Url, proxy, proxy_auth):
     try:
-        data = await get_data(url)
+        data = await get_data(url, proxy, proxy_auth)
     except Exception as ex:
         data = ()
         print(ex)
@@ -105,17 +107,45 @@ async def parse_url(url: Url):
 
 
 async def parse_cycle():
+    proxies_old = [
+        ("213.166.75.128", "9357", "kut1RP", "2p0SDV"),
+        ("213.166.73.115", "9528", "kut1RP", "2p0SDV"),
+        ("213.166.73.121", "9005", "kut1RP", "2p0SDV"),
+        ("213.166.73.21", "9585", "kut1RP", "2p0SDV"),
+        ("213.166.74.118", "9188", "kut1RP", "2p0SDV"),
+        ("194.67.218.97", "9494", "kut1RP", "2p0SDV"),
+        ("194.67.215.27", "9940", "kut1RP", "2p0SDV"),
+        ("194.67.213.84", "9821", "kut1RP", "2p0SDV"),
+        ("194.67.217.137", "9491", "kut1RP", "2p0SDV"),
+        ("194.67.216.82", "9532", "kut1RP", "2p0SDV")
+    ]
+    
+    proxies = (
+        ("186.65.115.199", "9918", "3VXHhu", "KjJBaM"),
+        ("186.65.114.242", "9140", "3VXHhu", "KjJBaM"),
+        ("186.65.117.3", "9107", "3VXHhu", "KjJBaM"),
+        ("186.65.114.225", "9715", "3VXHhu", "KjJBaM"),
+        ("186.65.117.222", "9780", "3VXHhu", "KjJBaM"),
+        ("138.59.206.102", "9492", "3VXHhu", "KjJBaM"),
+        ("138.59.205.211", "9675", "3VXHhu", "KjJBaM"),
+        ("138.59.207.16", "9907", "3VXHhu", "KjJBaM"),
+        ("138.59.204.78", "9187", "3VXHhu", "KjJBaM"),
+        ("138.59.206.152", "9248", "3VXHhu", "KjJBaM")
+    )
+
     i = 0
     while True:
-        if i % 10 == 0:
-            new_connection()
-        i += 1
+        prox = proxies[i % len(proxies)]
+
+        proxy = f'http://{prox[0]}:{prox[1]}'
+        proxy_auth = aiohttp.BasicAuth(prox[2], prox[3])
         urls = await Url.all()
         for url in urls:
             loop = asyncio.get_running_loop()
-            loop.create_task(parse_url(url))
+            loop.create_task(parse_url(url, proxy, proxy_auth))
 
         await asyncio.sleep(sleep_time)
+        i += 1
 
 
 @dp.message_handler(commands=['start'])
@@ -164,6 +194,21 @@ async def cancel_command(message: types.Message):
     del_actions(tg_id)
 
     await message.answer('Действие отменено')
+
+
+@dp.message_handler(commands=['test'])
+async def test_ip_command(message: types.Message):
+    if message.from_user.id not in admins:
+        return
+    new_connection()
+    connector = ProxyConnector.from_url('socks5://127.0.0.1:9050')
+    async with aiohttp.ClientSession(connector=connector) as session:
+        response = await session.get(
+            'http://httpbin.org/ip'
+            )
+        result = await response.text()
+
+    await message.answer(result)
 
 
 @dp.message_handler(commands=['list'])
@@ -244,10 +289,9 @@ async def listen_url(message: types.Message):
     del_actions(message.from_user.id)
 
 
-
 async def on_startup(dp):
     await db_init()
-    await bot.set_webhook(WEBHOOK_URL)
+    #await bot.set_webhook(WEBHOOK_URL)
     loop = asyncio.get_running_loop()
     loop.create_task(parse_cycle())
     print('start')
@@ -255,21 +299,19 @@ async def on_startup(dp):
 
 async def on_shutdown(dp):
     logger.warning('Shutting down..')
-    await bot.delete_webhook()
+    #await bot.delete_webhook()
     logger.warning('Bye!')
 
 
 if __name__ == '__main__':
-    try:
-        start_webhook(
-            dispatcher=dp,
-            webhook_path=WEBHOOK_PATH,
-            on_startup=on_startup,
-            on_shutdown=on_shutdown,
-            skip_updates=True,
-            host=WEBAPP_HOST,
-            port=WEBAPP_PORT,
-        )
-    except Exception as e:
-        print(e)
-        logger.exception('STOP_WEBHOOK')
+    while True:
+        try:
+            start_polling(
+                dp,
+                skip_updates=True,
+                on_startup=on_startup,
+                on_shutdown=on_shutdown,
+            )
+        except Exception as e:
+            print(e)
+            logger.exception('STOP_WEBHOOK')
--- parse.py
+++ parse.py
@@ -1,21 +1,23 @@
 import aiohttp
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup as bs4
+from aiohttp_proxy import ProxyConnector
 
 from db import Url
 
 user_agent = UserAgent()
-
-
-async def get_data(url: Url):
+async def get_data(url: Url, proxy, proxy_auth):
+    #connector = ProxyConnector.from_url('socks5://127.0.0.1:9050')
     async with aiohttp.ClientSession() as session:
         response = await session.get(
             url.url,
             headers={'User-Agent': user_agent.random},
-            proxy='socks5://127.0.0.1:9050'
+            proxy=proxy,
+            proxy_auth=proxy_auth
         )
         result = await response.text()
-
+    
+    print(url.url)
     soup = bs4(result, 'html.parser')
     size_div = soup.find('div', class_="product-size-selector__size-list-wrapper")
 
